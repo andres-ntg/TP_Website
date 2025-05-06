170e1a489d32a6aad0203407866956eb9931f17f
Andx45 <andres@nationalgt.com>
2025-05-06 12:19:14 -0600

    fix: Responsive footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -15,16 +15,12 @@ import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
 // importing icons for social media
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-
-
-
-
 export  function Footer(){
 
     return(
     <div>
       <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
-        <div className="flex w-full flex-col md:flex-row mx-10 mr-auto">
+        <div className="flex w-full flex-col md:flex-row mr-auto">
           <div className="w-full md:w-1/3 p-4 text-center text-white">
             <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
               <AuroraText speed={3}>TRAVEL PLACE</AuroraText>
@@ -39,23 +35,22 @@ export  function Footer(){
           </div>
 
           <div className="w-full md:w-1/3 p-4 text-center text-black">
-          <div className="flex justify-left space-x-6">
+            <div className="flex  md:flex-row md:justify-left justify-center items-center space-x-6">
               <a href="https://facebook.com/travelplacegt" target="_blank" rel="noopener noreferrer" className="text-purple-900 hover:text-purple-950">
-              <FontAwesomeIcon icon={faFacebook} size="2x" />
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
               </a>
               <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-purple-900 hover:text-purple-950">
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
               </a>
               <a href="https://instagram.com/@travelplacegt" target="_blank" rel="noopener noreferrer" className="text-purple-900 hover:text-purple-950">
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
               </a>
               <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-purple-900 hover:text-purple-950">
               <FontAwesomeIcon icon={faLinkedin} size="2x" />
               </a>
             </div>
-            <br />
-            <div className="flex flex-col items-start space-y-4">
-            {/* <h2 className="font-bold mb-2">Contáctanos</h2> */}
+            <br/>
+            <div className="flex flex-col items-center md:items-right space-y-4">
               <div className="flex items-center">
                 <FontAwesomeIcon icon={faEnvelope} className="text-gray-700 mr-3" />
                 <span>Correo: <a href="mailto:info@travelplace.com" className="text-purple-950">info@travelplace.com</a></span>
@@ -66,13 +61,12 @@ export  function Footer(){
               </div>
               <div className="flex items-center">
                 <FontAwesomeIcon icon={faMapMarkerAlt} className="text-gray-700 mr-3" />
-                <span>Dirección: <a href="" className="text-purple-950">Centro Empresarial <br/> San Cristobal Ciudad de Mixco</a> </span>
+                <span>Dirección: <a href="" className="text-purple-950">Centro Empresarial <br />San Cristobal Ciudad de Mixco</a></span>
               </div>
             </div>
-          </div>
-          
+          </div>       
         </div>
-        <div className="absolute bottom-0 left-0 w-full -z-10 -mb-14">
+        <div className="absolute bottom-0 left-0 w-full -z-10 mb-0 md:-mb-25">
           <img src="/Travel-Place-footer-2.svg" alt="Travel Place Footer" className="w-full" />
         </div>
       </div>

c40c12b4bfc7796182285ecdbddea450638109d3
andres-ntg <andres@nationalgt.com>
2026-02-26 16:12:05 -0600

    Implementación analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -37,6 +39,7 @@ export default function RootLayout({
             {children}
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-J5GWHGM6YM" />
     </html>
   );
 }
